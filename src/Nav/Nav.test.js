6375a3883a472bc3bc48ec824d85ff88cae02907
rebeccaleighfoster <rebecca.leigh.foster@gmail.com>
2019-10-19 12:39:52 -0500

    initial

--- src/Nav/Nav.test.js
+++ src/Nav/Nav.test.js
@@ -4,6 +4,11 @@ import Nav from './Nav';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Nav />, div);
+  ReactDOM.render(
+    <BrowserRouter>
+    <Nav />
+    </BrowserRouter>
+     div
+    );
   ReactDOM.unmountComponentAtNode(div);
 });
